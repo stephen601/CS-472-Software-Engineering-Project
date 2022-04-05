5932201b98cb19cd47753271397127e5e7dca44f
MintPaw <jerusanders@gmail.com>
2022-04-04 19:26:28 -0600

    Massive graphical changes

--- code/frontend/client.js
+++ code/frontend/client.js
@@ -1,4 +1,13 @@
-//@todo limit data sending size for server
+//@todo limit data sending string size for server
+//@todo Fix receipt screen since now we have tickets from multiple shows
+//@todo Figure out the report screen in general
+//@todo Do show editor
+//@todo Do seat editor
+//@todo Pretty up the text fields some more
+//@todo Do transitions
+//@todo Do background for plain text fields
+
+let verboseLogging = false;
 
 let SCREEN_NONE = 0;
 let SCREEN_LOGIN = 1;
@@ -6,15 +15,16 @@ let SCREEN_WAITING_FOR_SERVER = 2;
 let SCREEN_SHOW_LIST = 3;
 let SCREEN_SEAT_LIST = 4;
 let SCREEN_PAYMENT_INFO = 5;
-let SCREEN_RECEIPT = 6;
-let SCREEN_SHOW_EDITOR = 7;
-let SCREEN_SEAT_EDITOR = 8;
-let SCREEN_REPORT = 9;
+let SCREEN_CART = 6;
+let SCREEN_RECEIPT = 7;
+let SCREEN_SHOW_EDITOR = 8;
+let SCREEN_SEAT_EDITOR = 9;
+let SCREEN_REPORT = 10;
 
 let app = {};
 app.pixiApp = null;
 app.ui = {};
-app.ui.size = {}
+app.ui.size = {};
 app.ui.mouse = {};
 app.ui.mouseDown = false;
 app.ui.mouseJustDown = false;
@@ -24,6 +34,7 @@ app.ui.size.x = 0;
 app.ui.size.y = 0;
 app.ui.pixiFields = [];
 app.ui.pixiInputFields = [];
+app.ui.pixiInputFieldBackgrounds = [];
 app.ui.pixiSprites = [];
 app.ui.selectedInputField = null;
 
@@ -34,6 +45,8 @@ app.showManager = {};
 app.showManager.shows = [];
 app.showManager.currentShowIndex = 0;
 app.showManager.currentSeatIndices = [];
+
+let cart = [];
 
 let theaterRows = 8;
 let theaterCols = 12;
@@ -75,30 +88,32 @@ let theaterCols = 12;
 
 function runClient() {
 	let pixiApp = new PIXI.Application({
-		width: 1280, //@todo make this dynamically resizing
-		height: 720,
-		backgroundColor: 0x2980b9
+		resizeTo: window,
+		backgroundColor: 0xFF262626
 	});
 
 	document.body.appendChild(pixiApp.view);
 
 	pixiApp.ticker.add(updateClient);
 	app.pixiApp = pixiApp;
-	app.ui.size.x = 1280;
-	app.ui.size.y = 720;
 }
 
 function updateClient(delta) {
 	let ui = app.ui;
 	let showManager = app.showManager;
 
+	ui.size.x = app.pixiApp.screen.width;
+	ui.size.y = app.pixiApp.screen.height;
+
 	let onFirstFrame = false;
 	if (ui.prevScreen != ui.currentScreen) {
 		for (let i = 0; i < ui.pixiFields.length; i++) app.pixiApp.stage.removeChild(ui.pixiFields[i]);
 		for (let i = 0; i < ui.pixiInputFields.length; i++) app.pixiApp.stage.removeChild(ui.pixiInputFields[i]);
+		for (let i = 0; i < ui.pixiInputFieldBackgrounds.length; i++) app.pixiApp.stage.removeChild(ui.pixiInputFieldBackgrounds[i]);
 		for (let i = 0; i < ui.pixiSprites.length; i++) app.pixiApp.stage.removeChild(ui.pixiSprites[i]);
 		ui.pixiFields = [];
 		ui.pixiInputFields = [];
+		ui.pixiInputFieldBackgrounds = [];
 		ui.pixiSprites = [];
 		ui.selectedInputField = false;
 
@@ -133,79 +148,94 @@ function updateClient(delta) {
 			//x.focus();
 			document.body.appendChild(app.inputHtmlElement);
 
-			//@todo Get a list of the shows
 			changeScreen(SCREEN_LOGIN);
+
+			//@server loadShows.php?after=2525278934
+			//ShowID1, ShowName1, ShowDate1
+			//ShowID2, ShowName2, ShowDate2
+			//...
 		}
 	} else if (ui.currentScreen == SCREEN_LOGIN) {
 		if (onFirstFrame) {
-			ui.userField = createInputTextField();
-			ui.userField.text = "username";
-			ui.userField.x = ui.size.x / 2 - ui.userField.width / 2;
-			ui.userField.y = ui.size.y / 2 - ui.userField.height / 2;
+			//@server user creation
+			ui.userField = createInputTextField("username");
 
-			ui.passField = createInputTextField();
-			ui.passField.text = "password";
-			ui.passField.x = ui.size.x / 2 - ui.passField.width / 2;
-			ui.passField.y = ui.userField.y + ui.userField.height + 10;
+			ui.passField = createInputTextField("password");
 
 			ui.loginButton = createTextButtonSprite("Login");
 			console.log(ui.loginButton.width);
-			ui.loginButton.x = ui.size.x / 2 - ui.loginButton.width / 2;
-			ui.loginButton.y = ui.passField.y + ui.passField.height + 10;
 		}
+		ui.userField.x = ui.size.x/2 - ui.userField.width/2;
+		ui.userField.y = ui.size.y*0.3 - ui.userField.height/2;
+
+		ui.passField.x = ui.size.x / 2 - ui.passField.width / 2;
+		ui.passField.y = ui.userField.y + ui.userField.height + 10;
+
+		ui.loginButton.x = ui.size.x / 2 - ui.loginButton.width / 2;
+		ui.loginButton.y = ui.passField.y + ui.passField.height + 10;
 
 		if (spriteClicked(ui.loginButton)) {
 			attemptLogin(ui.userField.text, ui.passField.text);
+			//@server login.php?username=Jeru&password=pass
+			//Fail: -1
+			//Sucess: UserID, isAdmin
 			changeScreen(SCREEN_WAITING_FOR_SERVER);
 		}
 	} else if (ui.currentScreen == SCREEN_WAITING_FOR_SERVER) {
 		if (onFirstFrame) {
-			let waitingText = createTextField();
-			waitingText.text = "Waiting...";
-			waitingText.x = ui.size.x / 2 - waitingText.width / 2;
-			waitingText.y = ui.size.y / 2 - waitingText.height / 2;
-			changeScreen(SCREEN_SHOW_LIST); //@todo(Jeru) Hack
+			ui.waitingText = createTextField();
+			ui.waitingText.text = "Waiting...";
+
+			changeScreen(SCREEN_SHOW_LIST); //@hack
 		}
+
+		ui.waitingText.x = ui.size.x / 2 - ui.waitingText.width / 2;
+		ui.waitingText.y = ui.size.y / 2 - ui.waitingText.height / 2;
 	} else if (ui.currentScreen == SCREEN_SHOW_LIST) {
 		if (onFirstFrame) {
 			ui.showButtons = [];
 
-			let yPos = 0;
 			for (let i = 0; i < showManager.shows.length; i++) {
-				let sprite = createTextButtonSprite(" ");
-				sprite.x = ui.size.x / 2 - sprite.width / 2;
-				sprite.y = yPos; //trying to lower the buttons so its not at the top center
-				yPos += sprite.height + 10;
+				let show = showManager.shows[i];
+				let sprite = createTextButtonSprite(show.name);
 				ui.showButtons.push(sprite);
 			}
-			ui.addButton = createTextButtonSprite("+"); //creating add show button
-			ui.addButton.x = ui.size.x * 0.5 - ui.addButton.width / 2 + 200;
-			ui.addButton.y = ui.size.y - ui.addButton.height - 10;
+			ui.addButton = createTextButtonSprite("Add show"); //creating add show button
 
 			ui.removeButton = createTextButtonSprite("-"); //creating remove show button
-			ui.removeButton.x = ui.size.x * 0.5 - ui.removeButton.width / 2 - 200;
-			ui.removeButton.y = ui.size.y - ui.removeButton.height - 10;
 		}
-		/*if (false) { //To make visible buttons only for admin *How to get result
-			ui.addButton.visible = false; //hiding add show button true
-			ui.removeButton.visible = false; //hiding remove show button if false 
-		} else {
-			ui.addButton.visible = true; //showing add show button if true
-			ui.removeButton.visible = true; //showing remove show button if true 
-		}*/
+
+		ui.addButton.x = ui.size.x*0.8 - ui.addButton.width/2;
+		ui.addButton.y = ui.size.y*0.9 - ui.addButton.height;
 		if (spriteClicked(ui.addButton)) { // if the button add show is clicked then.. 
+			//@server addShow.php
+			//ShowID
 			console.log("add");//***Need solution to add show
+			//@todo Go to the edit show screen
 		}
+
+		ui.removeButton.x = ui.size.x*0.3 - ui.removeButton.width/2;
+		ui.removeButton.y = ui.size.y*0.9 - ui.removeButton.height;
 		if (spriteClicked(ui.removeButton)) { // if the button removed show is clicked then.. 
 			console.log("remove");//***Need solution to remove show
 		}
+
+		let yPos = 0;
 		for (let i = 0; i < ui.showButtons.length; i++) {
 			let button = ui.showButtons[i];
+
+			button.x = ui.size.x/2 - button.width/2;
+			button.y = yPos + 100; //trying to lower the buttons so its not at the top center
+			yPos += button.height + 10;
 
 			if (spriteClicked(button)) {
 				selectShow(i);
 				changeScreen(SCREEN_SEAT_LIST);
-
+				//@server getSeats.php?showId=3
+				//SeatAvailable0, SeatPrice0
+				//SeatAvailable1, SeatPrice1
+				//...
+				//SeatAvailable95, SeatPrice95
 			}
 		}
 	} else if (ui.currentScreen == SCREEN_SEAT_LIST) {
@@ -213,121 +243,189 @@ function updateClient(delta) {
 			showManager.currentSeatIndices = [];
 			ui.seatButtons = [];
 
-			let pad = 10;
-			let seatWidth = 64;
-			let seatHeight = 64;
-			
-			
-			let offsetX = ui.size.x/2 - (theaterCols * (seatWidth+pad))/2
-			let offsetY = 20;
-
 			for (let y = 0; y < theaterRows; y++) {
 				for (let x = 0; x < theaterCols; x++) {
-					let sprite = createSeatSprite();
-					sprite.width = seatWidth;
-					sprite.height = seatHeight;
-					sprite.x = x * (sprite.width + pad) + offsetX;
-					sprite.y = y * (sprite.height + pad) + offsetY;
-					ui.seatButtons.push(sprite);
+					let button = createSeatSprite();
+					ui.seatButtons.push(button);
 				}
 			}
 
-			let button = createTextButtonSprite("Buy");
-			button.x = ui.size.x*0.5 - button.width/2;
-			button.y = ui.size.y - button.height - 10;
-			ui.buyButton = button;
+			ui.addToCartButton = createTextButtonSprite("Add to cart(and checkout)");
 		}
+
+		let seatWidth = 64;
+		let seatHeight = 64;
+		let pad = seatWidth * 0.156;
+
+		let totalWidth;
+		let totalHeight;
+		for (let i = 0; i < 999; i++) {
+			seatWidth *= 0.9;
+			seatHeight *= 0.9;
+			totalWidth = (theaterCols * (seatWidth+pad));
+			totalHeight = (theaterRows * (seatHeight+pad));
+			pad = seatWidth * 0.156;
+
+			if (
+				totalWidth <= ui.size.x*0.8 &&
+				totalHeight <= ui.size.y*0.8
+			) break;
+		}
+
+		let offsetX = ui.size.x/2 - totalWidth/2;
+		let offsetY = 20;
 
 		for (let i = 0; i < ui.seatButtons.length; i++) {
 			let button = ui.seatButtons[i];
 			button.tint = 0xFFFFFF;
+
+			let x = i % theaterCols;
+			let y = Math.floor(i / theaterCols);
+			button.width = seatWidth;
+			button.height = seatHeight;
+			button.x = x * (button.width + pad) + offsetX;
+			button.y = y * (button.height + pad) + offsetY;
+
 			if (showManager.currentSeatIndices.indexOf(i) != -1) button.tint = 0xFFFF00;
 			if (spriteClicked(button)) {
 				selectSeat(i);
 			}
-        }
+		}
 
 		if (showManager.currentSeatIndices.length == 0) {
-			ui.buyButton.visible = false;
+			ui.addToCartButton.visible = false;
 		} else {
-			ui.buyButton.visible = true;
+			ui.addToCartButton.visible = true;
 		}
+
+		ui.addToCartButton.x = ui.size.x*0.5 - ui.addToCartButton.width/2;
+		ui.addToCartButton.y = ui.size.y*0.9 - ui.addToCartButton.height;
+		if (spriteClicked(ui.addToCartButton)) {
+			let show = showManager.shows[showManager.currentShowIndex];
+
+			for (let i = 0; i < showManager.currentSeatIndices.length; i++) {
+				let cartEntry = {
+					showId: show.id,
+					seatIndex: showManager.currentSeatIndices[i]
+				};
+				cart.push(cartEntry);
+			}
+			changeScreen(SCREEN_CART);
+		}
+
+	} else if (ui.currentScreen == SCREEN_CART) {
+		if (onFirstFrame) {
+			ui.cartEntrySprites = [];
+
+			for (let i = 0; i < cart.length; i++) {
+				let entry = cart[i];
+				let sprite = createTextButtonSprite(entry.showId + ", " + entry.seatIndex); //@todo seatIndex should probably be a string like C6
+				ui.cartEntrySprites.push(sprite);
+			}
+
+			ui.backButton = createTextButtonSprite("Browse more shows");
+			ui.buyButton = createTextButtonSprite("Buy");
+		}
+
+		let yPos = ui.size.y*0.15;
+		for (let i = 0; i < ui.cartEntrySprites.length; i++) {
+			let sprite = ui.cartEntrySprites[i];
+
+			sprite.x = ui.size.x/2 - sprite.width/2;
+			sprite.y = yPos;
+
+			yPos += sprite.height + ui.size.y*0.02;
+		}
+
+		ui.backButton.x = ui.size.x*0.4 - ui.backButton.width/2;
+		ui.backButton.y = ui.size.y - ui.backButton.height - ui.size.y*0.15;
+		if (spriteClicked(ui.backButton)) {
+			changeScreen(SCREEN_SHOW_LIST);
+		}
+
+		ui.buyButton.x = ui.size.x*0.7 - ui.buyButton.width/2;
+		ui.buyButton.y = ui.size.y - ui.buyButton.height - ui.size.y*0.15;
 		if (spriteClicked(ui.buyButton)) {
 			changeScreen(SCREEN_PAYMENT_INFO);
 		}
 
 	} else if (ui.currentScreen == SCREEN_PAYMENT_INFO) {
 		if (onFirstFrame) {
-			ui.nameField = createInputTextField();
-			ui.nameField.text = "Name on card";
-			ui.nameField.x = ui.size.x / 2 - ui.nameField.width / 2;
-			ui.nameField.y = 20;
-
-			ui.creditNumber = createInputTextField();
-			ui.creditNumber.text = "Credit Card Number";
-			ui.creditNumber.x = ui.size.x / 2 - ui.creditNumber.width / 2;
-			ui.creditNumber.y = ui.nameField.y + ui.nameField.height + 10;
-
-			ui.cvvField = createInputTextField();
-			ui.cvvField.text = "CVV";
-			ui.cvvField.x = ui.size.x / 2 - ui.cvvField.width / 2;
-			ui.cvvField.y = ui.creditNumber.y + ui.creditNumber.height + 10;
-
-			ui.dateField = createInputTextField();
-			ui.dateField.text = "Date";
-			ui.dateField.x = ui.size.x / 2 - ui.dateField.width / 2;
-			ui.dateField.y = ui.cvvField.y + ui.cvvField.height + 10;
+			//@server Possibly load saved credit card info
+			ui.nameField = createInputTextField("Name on card");
+			ui.creditNumber = createInputTextField("Credit Card Number");
+			ui.cvvField = createInputTextField("CVV");
+			ui.dateField = createInputTextField("Date");
 			// todo exp date picker
 
-			ui.zipField = createInputTextField();
-			ui.zipField.text = "Zip";
-			ui.zipField.x = ui.size.x / 2 - ui.zipField.width / 2;
-			ui.zipField.y = ui.dateField.y + ui.dateField.height + 10;
+			ui.zipField = createInputTextField("Zip");
 
-			let button = createTextButtonSprite("Buy");
-			button.x = ui.size.x * 0.5 - button.width / 2;
-			button.y = ui.size.y - button.height - 10;
-			ui.buyButton = button;
+			ui.buyButton = createTextButtonSprite("Buy");
 		}
 
+		let downPad = ui.size.y*0.02;
+		ui.nameField.x = ui.size.x / 2 - ui.nameField.width / 2;
+		ui.nameField.y = ui.size.y*0.15;
+
+		ui.creditNumber.x = ui.size.x / 2 - ui.creditNumber.width / 2;
+		ui.creditNumber.y = ui.nameField.y + ui.nameField.height + downPad;
+
+		ui.cvvField.x = ui.size.x / 2 - ui.cvvField.width / 2;
+		ui.cvvField.y = ui.creditNumber.y + ui.creditNumber.height + downPad;
+
+		ui.dateField.x = ui.size.x / 2 - ui.dateField.width / 2;
+		ui.dateField.y = ui.cvvField.y + ui.cvvField.height + downPad;
+
+		ui.zipField.x = ui.size.x / 2 - ui.zipField.width / 2;
+		ui.zipField.y = ui.dateField.y + ui.dateField.height + downPad;
+
+		ui.buyButton.x = ui.size.x * 0.5 - ui.buyButton.width / 2;
+		ui.buyButton.y = ui.size.y*0.85 - ui.buyButton.height;
 		if (spriteClicked(ui.buyButton)) {
-			chargeCreditCard(ui.nameField.text, ui.creditNumber.text, ui.dateField.text, 0); //@todo Figure out the price
+			chargeCreditCard(ui.nameField.text, ui.creditNumber.text, ui.dateField.text, 0); //@todo Figure out the price // The server probably has to do this
 			//changeScreen(SCREEN_WAITING_FOR_SERVER); //@todo(Jeru): Make this actually work
 			changeScreen(SCREEN_RECEIPT);
-        }
+			//@server buyTickets.php?seats=3,31,3,46,2,25,1,73 // These are interleaved showIds and seat numbers
+			//Success: 1
+			//Fail: 0,reasonString
+		}
 	} else if (ui.currentScreen == SCREEN_RECEIPT) {
 		//@todo We need name, age, address, telephone, and email, but the user profile should already have that.
 		if (onFirstFrame) {
 			let show = showManager.shows[showManager.currentShowIndex];
 
-			let nameField = createTextField();
-			nameField.text = show.name;
-			nameField.x = ui.size.x / 2 - nameField.width / 2;
-			nameField.y = 20;
+			ui.nameField = createTextField();
+			ui.nameField.text = show.name;
 
-			let showDateField = createTextField();
-			showDateField.text = show.date.toString();
-			showDateField.x = ui.size.x / 2 - showDateField.width / 2;
-			showDateField.y = nameField.y + nameField.height + 10;
+			ui.showDateField = createTextField();
+			ui.showDateField.text = show.date.toString();
 
-			let seatsField = createTextField();
-			seatsField.text = "Seats: ";
+			ui.seatsField = createTextField();
+			ui.seatsField.text = "Seats: ";
 			for (let i = 0; i < showManager.currentSeatIndices.length; i++) {
 				let seatIndex = showManager.currentSeatIndices[i];
 				let col = seatIndex % theaterCols;
 				let row = Math.floor(seatIndex / theaterCols);
-				seatsField.text += String.fromCharCode(65 + row);
-				seatsField.text += col + 1;
-				if (i < showManager.currentSeatIndices.length-1) seatsField.text += ", ";
+				ui.seatsField.text += String.fromCharCode(65 + row);
+				ui.seatsField.text += col + 1;
+				if (i < showManager.currentSeatIndices.length-1) ui.seatsField.text += ", ";
 			}
-			seatsField.x = ui.size.x / 2 - seatsField.width / 2;
-			seatsField.y = showDateField.y + showDateField.height + 10;
 
-			let button = createTextButtonSprite("Done");
-			button.x = ui.size.x * 0.5 - button.width / 2;
-			button.y = ui.size.y - button.height - 10;
-			ui.doneButton = button;
+			ui.doneButton = createTextButtonSprite("Done");
 		}
+
+		let downPad = ui.size.y*0.02;
+		ui.nameField.x = ui.size.x / 2 - ui.nameField.width / 2;
+		ui.nameField.y = ui.size.y*0.15;
+
+		ui.showDateField.x = ui.size.x / 2 - ui.showDateField.width / 2;
+		ui.showDateField.y = ui.nameField.y + ui.nameField.height + downPad;
+
+		ui.seatsField.x = ui.size.x / 2 - ui.seatsField.width / 2;
+		ui.seatsField.y = ui.showDateField.y + ui.showDateField.height + downPad;
+
+		ui.doneButton.x = ui.size.x * 0.5 - ui.doneButton.width / 2;
+		ui.doneButton.y = ui.size.y*0.85 - ui.doneButton.height;
 
 		if (spriteClicked(ui.doneButton)) {
 			changeScreen(SCREEN_SHOW_LIST);
@@ -336,19 +434,33 @@ function updateClient(delta) {
 		if (onFirstFrame) {
 
 		}
+		//@todo Delete show
+		//@server editShow.php?showId=3&showName=MyShowName&showDate=3248092347
+		// 1
 	} else if (ui.currentScreen == SCREEN_SEAT_EDITOR) {
 		if (onFirstFrame) {
 
 		}
+		//@server editSeats.php?showId=3&seats=2,52,26,12&price=5
+		// 1
 	} else if (ui.currentScreen == SCREEN_REPORT) {
 		if (onFirstFrame) {
-
+			//@server getAllReceipts.php
 		}
 	}
 
 	{ /// Update input field
 		for (let i = 0; i < ui.pixiInputFields.length; i++) {
+			let sprite = ui.pixiInputFieldBackgrounds[i];
 			let field = ui.pixiInputFields[i];
+
+			let pad = 5;
+			sprite.width = field.width + pad;
+			sprite.height = field.height + pad;
+
+			sprite.x = field.x - pad/2;
+			sprite.y = field.y - pad/2;
+
 			if (field == ui.selectedInputField) {
 				field.alpha = 0.75;
 			} else {
@@ -388,22 +500,39 @@ function createTextField() {
 	return field;
 }
 
-function createInputTextField() {
+function createInputTextField(hintText) {
+	if (verboseLogging) console.log("Input text field created");
+	let sprite = new PIXI.NineSlicePlane(PIXI.Texture.from("assets/nineSliceButton.png"), 32, 32, 32, 32);
+	sprite.tint = 0xFFE0E0E0;
+	app.pixiApp.stage.addChild(sprite);
+	app.ui.pixiInputFieldBackgrounds.push(sprite);
+
 	let field = new PIXI.Text();
+	field.text = hintText;
 	app.pixiApp.stage.addChild(field);
 	app.ui.pixiInputFields.push(field);
 	return field;
 }
 
 function createTextButtonSprite(text) {
-	let sprite = new PIXI.Sprite.from("assets/button.png"); //@todo(Jeru): Cache textures so that I know the size of them on frame 1
+	let field = new PIXI.Text();
+	field.text = text;
+
+	let sprite = new PIXI.NineSlicePlane(PIXI.Texture.from("assets/nineSliceButton.png"), 32, 32, 32, 32);
+	sprite.width = field.width;
+	sprite.height = field.height;
+	sprite.tint = 0xFF47C1F5;
+
+	sprite.addChild(field);
+
 	app.pixiApp.stage.addChild(sprite);
 	app.ui.pixiSprites.push(sprite);
 	return sprite;
 }
 
 function createSeatSprite() {
-	let sprite = new PIXI.Sprite.from("assets/seat.png");
+	// let sprite = new PIXI.Sprite.from("assets/seat.png");
+	let sprite = new PIXI.NineSlicePlane(PIXI.Texture.from("assets/nineSliceButton.png"), 32, 32, 32, 32);
 	app.pixiApp.stage.addChild(sprite);
 	app.ui.pixiSprites.push(sprite);
 	return sprite;
@@ -442,7 +571,7 @@ function attemptLogin(username, password) {
 	// 35, Jeru Sanders, jerusanders@gmail.com
 	//xmlhttp.open("GET", "login.html", true);
 	//@stp xmlhttp.onerror
-	xmlhttp.open("GET", "login.php?username="+username+"&password="+password, true);
+	xmlhttp.open("GET", "includes/login.inc.php?username="+username+"&password="+password, true);
 	xmlhttp.send();
 }
 
--- code/frontend/run.bat
+++ code/frontend/run.bat
@@ -2,4 +2,3 @@ xcopy "." "C:\xampp\htdocs\theater" /s /e /y
 
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://127.0.0.1/theater
 "C:\Program Files\Google\Chrome\Application\chrome.exe" http://127.0.0.1/theater
-pause
