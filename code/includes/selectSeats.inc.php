b7b594a57899a0a55e8e1fbb70d63a5ee5393c8f
stephen601 <71991128+stephen601@users.noreply.github.com>
2022-04-14 09:59:08 -0600

    Update selectSeats.inc.php

--- code/includes/selectSeats.inc.php
+++ code/includes/selectSeats.inc.php
@@ -1,1 +1,17 @@
-Error updating record: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'HERE ShowID =  & SeatID =' at line 1 
+<?php
+
+include 'connection.inc.php';
+$Showid=$_GET['ShowID'];
+$Seatid=$_GET['SeatID'];
+
+
+$sql = "UPDATE Seats SET ShowID='$Showid',SeatID='$Seatid',SeatPrice='5',SeatVacancy='1' WHERE ShowID = $Showid AND SeatID =$Seatid " ;
+
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record updated successfully";
+  }
+ else {
+  echo "Error updating record: " . $conn->error;
+}
+?> 
