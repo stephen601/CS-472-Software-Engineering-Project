70d4d749dceb4e20b20e079758e1a2cf96b356ae
stephen601 <71991128+stephen601@users.noreply.github.com>
2022-04-14 09:55:22 -0600

    Add files via upload

--- code/includes/loadSeats.inc.php
+++ code/includes/loadSeats.inc.php
@@ -1,1 +1,18 @@
-NO Seats created 
+<?php
+
+include 'connection.inc.php';
+$Showid=$_GET['ShowID'];
+
+$sql="SELECT ShowID, SeatID, SeatPrice, SeatVacancy FROM Seats WHERE ShowID = $Showid";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo " ShowID: " . $row["ShowID"]. "  - SeatID: " . $row["SeatID"]. "S eatPrice: " . $row["SeatPrice"]. " -  SeatVacancy: " . $row["SeatVacancy"]. " <br>";
+  }
+} else {
+  echo "NO Seats created";
+}
+$conn->close();
+?> 
