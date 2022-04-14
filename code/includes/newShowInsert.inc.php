04e539474096c87789c166c9114080c6be06ac1d
stephen601 <71991128+stephen601@users.noreply.github.com>
2022-04-14 09:57:53 -0600

    Update newShowInsert.inc.php

--- code/includes/newShowInsert.inc.php
+++ code/includes/newShowInsert.inc.php
@@ -1,1 +1,19 @@
-Event is already in the system. Please select different Event and try again.
+<?php
+include 'shows.inc.php';
+$showname=$_GET['showname'];
+$showdate=$_GET['showdate'];
+$showtime=$_GET['showtime'];
+$showprice=$_GET['showprice'];
+$newShow=new Shows;
+$newShow->ShowName=$showname;
+$newShow->ShowDate=$showdate;
+$newShow->ShowTime=$showtime;
+$newShow->ShowPrice=$showprice;
+$test=$newShow->checkShow();
+//var_dump($test);
+if($test==TRUE){
+    $results= $newShow->setShow();
+}else{
+    echo "Event is already in the system. Please select different Event and try again.";
+}
+
