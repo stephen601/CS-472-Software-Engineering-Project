3a84fc9968101b8163f1988962e397f435586923
stephen601 <71991128+stephen601@users.noreply.github.com>
2022-04-14 09:57:22 -0600

    Update login.inc.php

--- code/includes/login.inc.php
+++ code/includes/login.inc.php
@@ -1,1 +1,8 @@
-False<br> Username does not exist.
+<?php
+include 'userProfile.inc.php';
+$username = $_GET['username'];
+$password = $_GET['password'];
+$userPro = new UserProfile;
+$userPro->Username=$username;
+$userPro->Password=$password;
+echo $userPro->getUsername(); 
