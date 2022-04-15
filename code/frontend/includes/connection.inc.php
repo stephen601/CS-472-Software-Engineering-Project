a33ca956426d933593fe2a7660849759e6dd1b17
MintPaw <jerusanders@gmail.com>
2022-04-15 09:20:34 -0600

    Made it so the connection works again hopefully

--- code/frontend/includes/connection.inc.php
+++ code/frontend/includes/connection.inc.php
@@ -1,19 +1,25 @@
 <?php
   $servername = "localhost";
-// if (isLocal) {
-// } else {
-// }
-  // $username = "id18705581_wp_9602beb976f1a3354f5535db232f0210";
-  // $password = "ZW5tdQ======";
-  // $database = "id18705581_wp_9602beb976f1a3354f5535db232f0210";
-  // $port = 3306;
 
   $username = "root";
   $password = "";
   $database = "test";
   $port = 3307;
+  $conn = mysqli_connect($servername, $username, $password, $database, $port);
 
-  return $conn = mysqli_connect($servername, $username, $password, $database, $port);
+	if (!$conn) {
+		$username = "id18705581_wp_9602beb976f1a3354f5535db232f0210";
+		$password = "ZW5tdQ======";
+		$database = "id18705581_wp_9602beb976f1a3354f5535db232f0210";
+		$port = 3306;
+		$conn = mysqli_connect($servername, $username, $password, $database, $port);
+	}
+
+	if (!$conn) {
+		die("Database error\n");
+	}
+
+	return $conn;
   
 	 /*
 		 function conn($servername, $username, $password, $database){
